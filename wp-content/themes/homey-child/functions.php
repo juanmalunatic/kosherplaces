6c2f9f31b18a99fcf554f1e8ba421b38414cc02c
Juan Manuel Luna López <lunalopezjm@gmail.com>
2021-04-28 23:10:17 -0500

    Add system to override JavaScript files w/o modifying the scripts themeselves (checkpoint)

--- wp-content/themes/homey-child/functions.php
+++ wp-content/themes/homey-child/functions.php
@@ -13,9 +13,11 @@ add_action('wp_enqueue_scripts', 'homey_enqueue_styles');
 $path_child_theme = get_stylesheet_directory();
 
 // Partial overrides for placehold.it
-require($path_child_theme . '/placeholdit-override.php');
+require($path_child_theme . '/override__placeholdit.php');
 
 // Overrides for add-listing function
-require($path_child_theme . '/add-listing-override.php');
+require($path_child_theme . '/override__add-listing.php');
 
+// Overrides for parent theme JS scripts
+require($path_child_theme . '/override__javascript.php')
 ?>
